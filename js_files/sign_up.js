8bebd8b7498cc630d7b41900ebd66b5d27c9113f
sumeetCodes <sumeet71906981@gmail.com>
2024-11-16 17:45:31 +0000

    unnecessary code removed

--- js_files/sign_up.js
+++ js_files/sign_up.js
@@ -1,28 +1,4 @@
 
-// let getUserEmail = document.getElementById('userInputEmail')
-// let getUserPassword = document.getElementById('userInputPassword')
-
-// let Form = document.getElementById('inputMailPassForm')
-
-// let errorElement = document.getElementById('error')
-
-// Form.addEventListener('submit', (e) => {
-//     let users_Email_Password =[]
-//     if(getUserEmail.value === '' || getUserEmail == null){
-//         users_Email_Password.push('Email is required, put correct Email address')
-//     }
-
-
-//     if (users_Email_Password.length > 0) {
-//         e.preventDefault()
-//        errorElement.innerHTML = users_Email_Password.join(',')
-//     }
-
- 
-// })
-
-
-
 // Adds an event listener to the form that triggers on form submission
 document.getElementById("inputMailPassForm").addEventListener("submit" , function (e) {
 
